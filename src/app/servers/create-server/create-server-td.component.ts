19690fcb1a53890e1910269675be3a8ff65a6113
Emilio Calejero <milioce@gmail.com>
2020-09-15 13:21:04 +0200

    10 Forms - Add TD to form: name, ngSubmit, ngModel

--- src/app/servers/create-server/create-server-td.component.ts
+++ src/app/servers/create-server/create-server-td.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-create-server-td',
@@ -18,4 +19,22 @@ export class CreateServerTdComponent implements OnInit {
   onCancel() {
     this.cancel.emit(true);
   }
+
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+  }
+
+  onDebug(form: NgForm) {
+    console.log('Form', form);
+    console.log('form value', form.value);
+    console.log('form touched', form.touched);
+    console.log('form valid', form.valid);
+    console.log('form errors', form.errors);
+
+    console.log('form Name touched', form.controls.name.touched);
+    console.log('form Name dirty', form.controls.name.dirty);
+    console.log('form Name valid', form.controls.name.valid);
+    console.log('form Name errors', form.controls.name.errors);
+  }
+
 }
