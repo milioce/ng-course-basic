cfb9b8baf83c3d4fb0c50c89e52e7f8bdc57aea4
Emilio Calejero <milioce@gmail.com>
2020-09-17 12:22:41 +0200

    11 Router - Passing parameters II

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,20 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: string, name: string};
+  subscription: Subscription;
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.user = {
-      id: this.route.snapshot.params['id'],
-      name: this.route.snapshot.params['name']
+    this.subscription = this.route.params.subscribe((params: Params) => {
+      this.user = {
+        id: params['id'],
+        name: params['name']
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
     }
   }
 
